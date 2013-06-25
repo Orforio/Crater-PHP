cf64c1f93fb7e328f0c79accaf6258a5c88e6798
PkerUNO <pkeruno@gmail.com>
2013-06-26 00:49:11 +0100

    Added "add/remove row" functionality

--- app/webroot/js/crater.js
+++ app/webroot/js/crater.js
@@ -1,5 +1,5 @@
 function onSortableUpdate(event, ui) {
-	var newTrackOrder = $('#editForm tbody').sortable('toArray', {attribute: 'data-trackorder'});	// Array of new order
+	var newTrackOrder = $('#editForm tbody').sortable('toArray', {attribute: 'data-trackorder'});	// TODO: remove
 	console.debug(newTrackOrder);
 	
 	$('#editForm tbody tr').each(function(currentTrackIndex) {
@@ -29,4 +29,25 @@ function updateSort(currentTrackIndex, newTrackOrder, currentRow) {
 //	var newTrackOrderNumber = (jQuery.inArray(currentTrackOrder, newTrackOrder)) + 1;
 	$(currentRow).find('td .setlist_order').html(' ' + currentTrackOrder);
 	$(currentRow).find('input[id$="SetlistOrder"]').attr('value', currentTrackOrder);
+}
+
+function addTrackRow() {
+	var numberRows = $('#editForm tbody tr').length;
+	var newRowNumber = numberRows + 1;
+	var newRowIndex = numberRows;
+	var newRow = '<tr>' +
+			'<input type="hidden" name="data[Track][' + newRowIndex + '][setlist_order]" value="' + newRowNumber + '" id="Track' + newRowIndex + 'SetlistOrder">' +
+			'<td class="draggable"><i class="icon-resize-vertical"></i><label class="setlist_order"> ' + newRowNumber + '</label></td>' +
+			'<td><input name="data[Track][' + newRowIndex + '][artist]" placeholder="Artist" type="text" id="Track' + newRowIndex + 'Artist"></td>' +
+			'<td><input name="data[Track][' + newRowIndex + '][title]" placeholder="Title" type="text" id="Track' + newRowIndex + 'Title" required="required"></td>' +
+			'<td><input name="data[Track][' + newRowIndex + '][label]" class="input-small" placeholder="Label" type="text" id="Track' + newRowIndex + 'Label"></td>' +
+			'<td><input name="data[Track][' + newRowIndex + '][length]" class="input-mini" placeholder="00:00" type="text" id="Track' + newRowIndex + 'Length"></td>' +
+			'<td><input name="data[Track][' + newRowIndex + '][bpm_start]" class="input-mini" placeholder="BPM" type="text" id="Track' + newRowIndex + 'BpmStart"></td>' +
+			'<td><input name="data[Track][' + newRowIndex + '][key_start]" class="input-mini" placeholder="Key" type="text" id="Track' + newRowIndex + 'KeyStart"></td>' +
+		'</tr>';
+	$('#editForm tbody').append(newRow);
+}
+
+function removeTrackRow() {
+	$('#editForm tbody tr:last').remove();
 }
