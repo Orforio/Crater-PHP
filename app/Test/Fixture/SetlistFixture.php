5894acc9c1ca9f4726a5427153f924b5af33e09b
PkerUNO <pkeruno@gmail.com>
2013-07-29 01:32:11 +0100

    Correct data - Setlist 2 Master BPM

--- app/Test/Fixture/SetlistFixture.php
+++ app/Test/Fixture/SetlistFixture.php
@@ -12,7 +12,7 @@ class SetlistFixture extends CakeTestFixture {
       
 	public $records = array(
 		array('id' => 1, 'name' => 'Sunset Trance I: Beachline Expressway', 'author' => 'TGGR.EXE', 'genre' => 'Trance', 'modified' => '2013-07-27 01:27:04', 'private_key' => '', 'master_bpm' => '140'),
-		array('id' => 2, 'name' => 'Trancemission III', 'author' => 'PkerUNO', 'genre' => 'Trance', 'modified' => '2013-07-27 01:27:04', 'private_key' => '', 'master_bpm' => '138'),
+		array('id' => 2, 'name' => 'Trancemission III', 'author' => 'PkerUNO', 'genre' => 'Trance', 'modified' => '2013-07-27 01:27:04', 'private_key' => '', 'master_bpm' => '180'),
 		array('id' => 3, 'name' => 'Bemix', 'author' => 'DJ Konami', 'genre' => 'Beatmania', 'modified' => '2013-07-27 01:27:04', 'private_key' => '', 'master_bpm' => ''),
 		array('id' => 4, 'name' => 'House Party', 'author' => 'Noel', 'genre' => 'House', 'modified' => '2013-07-27 01:27:04', 'private_key' => '', 'master_bpm' => '')
 	);
