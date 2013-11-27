eafc6be340bf02df167124f7bb0b4ed9cbc20a1f
Richard Whittaker <pkeruno@gmail.com>
2013-11-27 22:41:16 +0000

    Add Key fixtures from live database

---
+++ app/Test/Fixture/KeyFixture.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * KeyFixture
+ *
+ */
+class KeyFixture extends CakeTestFixture {
+
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('model' => 'Key', 'records' => true);
+
+}
+?>
